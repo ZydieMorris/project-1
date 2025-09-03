65a4645a39883ba5e04e220f7858fcbac03c1d27
NinyoRaleigh <ninoraleighparagas@gmail.com>
2025-09-03 15:09:58 +0800

    I fixed the header

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
  <div class=" space-y-8 " >
   <div class=" space-x-2 p-2  absolute top-10">
     <RouterLink to="/home" class="bg-blue-500 text-white rounded-lg p-5 text-lg">Home Page</RouterLink>
-    <RouterLink to="/about" class="bg-black text-white rounded-lg p-5 text-lg">About Page</RouterLink>
+    <RouterLink to="/about" class="bg-red-500 text-white rounded-lg p-5 text-lg">About Page</RouterLink>
   </div>
  
 <router-view></router-view>
