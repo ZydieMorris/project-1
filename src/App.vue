33c622670575e4beac7dbb30a45dc091ba645398
ZydieMorris <cansanciozydie@gmail.com>
2025-09-03 14:57:43 +0800

    i fixed the nav bar color

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,10 @@
 <template>
  <div class=" space-y-8 " >
   <div class=" space-x-2 p-2  absolute top-10">
-    <RouterLink to="/home" class="bg-black text-white rounded-lg p-5 text-lg">Home Page</RouterLink>
+    <RouterLink to="/home" class="bg-blue-500 text-white rounded-lg p-5 text-lg">Home Page</RouterLink>
     <RouterLink to="/about" class="bg-black text-white rounded-lg p-5 text-lg">About Page</RouterLink>
   </div>
  
-    
-
- <div class=" flex  space-x-8 border h-130 w-250 p-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-
-  <div>
-    <img class="rounded full h-70 w-70" src="/images/profile.jpg"/>
-  </div>
-  
-    <div class="space-y-5 bg-red-500 h-30 ">
-      <div>
-         <p class="text-5xl font-bold flex whitespace-nowrap">Zydie Morris A. Cansancio</p>
-      <p class="text-gray-400 text-xl ">sid@gmail.com</p>
-
-      <div class="space-y-20">
-        <p>Social Media Accounts</p>
-        
-      </div>
-      </div>
-     
-    </div>
-    
- 
-    
- </div>
-
-
-
 <router-view></router-view>
 
 </div>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,6 +5,27 @@
 
 <template>
 
-<h1>Home Page</h1>
+    <div class=" flex  space-x-8 border h-130 w-250 p-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+
+        <div>
+            <img class="rounded full h-70 w-70" src="/images/profile.jpg" />
+        </div>
+
+        <div class="space-y-5 bg-red-500 h-30 ">
+            <div>
+                <p class="text-5xl font-bold flex whitespace-nowrap">Zydie Morris A. Cansancio</p>
+                <p class="text-gray-400 text-xl ">sid@gmail.com</p>
+
+                <div class="space-y-20">
+                    <p>Social Media Accounts</p>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
 
 </template>
